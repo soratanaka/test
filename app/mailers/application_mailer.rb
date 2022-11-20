6fe3660c98d394be8a4f181117732d8653ddd63b
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-20 16:58:34 +0900

    test

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'rari4423@gmail.com'
+  layout 'mailer'
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :blogs
+  root 'blogs#index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
